09c67febd53053b4810c842d8adfb11cb257d1c2
Mobolaji <bjwealthy@gmail.com>
2025-04-30 09:53:19 +0100

    student library and assignment components added

--- src/pages/Students/StudentAnnouncement.jsx
+++ src/pages/Students/StudentAnnouncement.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from './Sidebar';
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:5000/api/v1/';
+import {AnnouncementContainer, SidebarContainer, Content, AnnouncementHeader, AnnouncementList, AnnouncementItem, AnnouncementTitle, AnnouncementContent} from '../../styles/AnnouncementStyles';
 
 const StudentAnnouncement = () => {
   return (
-    <div>
-      Anouncements appear here soon
-    </div>
+    <AnnouncementContainer>
+      <SidebarContainer>
+        <Sidebar />
+      </SidebarContainer>
+      <Content>
+        <AnnouncementHeader>Announcements</AnnouncementHeader>
+        <AnnouncementList>
+          {/* dfghjkl */}
+        </AnnouncementList>
+      </Content>
+    </AnnouncementContainer>
   )
 }
 
